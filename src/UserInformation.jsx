d27870cb16ff1f143b858287e312d3a6839b9740
ana teixeira <ana.martins@external.thalesgroup.com>
2018-12-04 11:43:00 +0000

    answer when user does not exist

--- src/UserInformation.jsx
+++ src/UserInformation.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
 
-const UserInformation = props => (!props.name) ? (
-  <p>
-    (You will see user information here)
-  </p>
-) : (<div>
-  <h1>Here is your Lovely user's info</h1><br />
-  <div className="line">Name: {props.name}</div>
-  <div className="line">Repos:</div>
-  {<RepositoriesInfo user={props.login} />}
-</div>);
+const UserInformation = props => { console.log(props)
+  if (!props.name) {
+    return (props.login!="" && !props.login) ?
+      (<p>
+        Oh no! The given user doesn't have github account &#9785;
+      </p>
+      ) :
+      (<p>
+        (You will see user information here)
+      </p>)
+  } else {
+    return (<div>
+      <h1>Here is your Lovely user's info</h1><br />
+      <div className="line">Name: {props.name}</div>
+      <div className="line">Repos:</div>
+      {<RepositoriesInfo user={props.login} />}
+    </div>)
+  }
+};
 
 class RepositoriesInfo extends React.Component {
 
@@ -35,7 +44,7 @@ class RepositoriesInfo extends React.Component {
       <div className="line">
         <ul>
           {this.state.repos.map(item => (
-            <li key={item.id}>{item.name}   <div className="description">  &#10140; {(item.description)? item.description: '\u26A0 (Ooops! Someone forgot the repo description ...)'  }</div></li>
+            <li key={item.id}>{item.name}   <div className="description">  &#10140; {(item.description) ? item.description : '\u26A0 (Ooops! Someone forgot the repo description ...)'}</div></li>
           ))}
         </ul>
       </div>
